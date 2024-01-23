06d2747f14b642d127ae317182141c46c0767ab2
Juan Diego <12-10566@usb.ve>
2024-01-23 15:41:21 -0400

    finalizadas validaciones del componente de personas

--- src/app/modules/people/add-person/add-person.component.ts
+++ src/app/modules/people/add-person/add-person.component.ts
@@ -18,15 +18,15 @@ export class AddPersonComponent {
     name: new FormControl('', Validators.required),
     lastname: new FormControl('',Validators.required),
     hasOlderChildren: new FormControl(false),
-    amountOfChildren: new FormControl(0),
+    amountOfChildren: new FormControl(0,Validators.min(0)),
 
   });
   addChildForm = new FormGroup( {
     name: new FormControl('', Validators.required),
     lastname: new FormControl('',Validators.required),
     hasFavoriteMovies: new FormControl(false),
-    amountOfMovies: new FormControl(0,Validators.min(19)),
-    age: new FormControl(0,[Validators.required,Validators.min(19)])
+    amountOfMovies: new FormControl(0,Validators.min(0)),
+    age: new FormControl(17,[Validators.required,Validators.min(1),Validators.max(17)])
 
 
 
@@ -36,7 +36,7 @@ export class AddPersonComponent {
     name: new FormControl('', Validators.required),
     director: new FormControl('', Validators.required),
     year: new FormControl(1999, Validators.required),
-    hasOscar: new FormControl(false, Validators.required),
+    hasOscar: new FormControl(false)
   })
 
   emptyError: boolean = false;
@@ -60,23 +60,30 @@ export class AddPersonComponent {
   ngOnInit() {
 
   }
+  /**
+   * Method that send the information of the person
+   */
   sendPersonData() {
 
     console.log(this.addPersonForm.value);
+    this.person.name = this.addPersonForm.value.name!;
+    this.person.lastname = this.addPersonForm.value.lastname!;
     if(!this.addPersonForm.invalid) {
-      this.person.name = this.addPersonForm.value.name!;
-      this.person.lastname = this.addPersonForm.value.lastname!;
-      this.people.push(this.person);
+      console.log("la persona:",this.person);
+
       this.amountOfChildren = this.addPersonForm.value.amountOfChildren!;
+      //Validate if the person has children
       if(this.amountOfChildren > 0) {
         this.amountOfChildrenRegistered = 1;
         this.hasSentPersonalInformation = true;
+        this.hasSentChildInformation = false;
         this.hasOlderChildren = true;
       } else {
+        this.people.push(this.person);
         let successMessage = "La persona se ha agregado satisfactoriamente.";
         this.addPersonForm.value.hasOlderChildren = false;
         this._generalService.setSuccessfulMessage(successMessage)
-        localStorage.setItem("people",JSON.stringify(this.people));
+        //localStorage.setItem("people",JSON.stringify(this.people));
         this.addPersonForm.reset();
         this.person = {'name': '', 'lastname' : '', 'children': []};
 
@@ -91,16 +98,21 @@ export class AddPersonComponent {
 
   }
 
+  /**
+   * Method that sends the information of the Child
+   */
   sendChildData() {
     console.log(this.addChildForm.value);
     if(!this.addChildForm.invalid) {
       this.child.name = this.addChildForm.value.name!;
       this.child.lastname = this.addChildForm.value.lastname!;
       this.child.amountOfMovies = this.addChildForm.value.amountOfMovies!;
-      this.amountOfMovies = this.addChildForm.value.amountOfMovies!
       this.child.age = this.addChildForm.value.age!
+      this.amountOfMovies = this.addChildForm.value.amountOfMovies!;
+      console.log("el hijo:",this.child);
+      //this.person.children = this.children;
       this.amountOfChildrenRegistered += 1;
-      this.children.push(this.child);
+
 
       //Validate if it's not  the last child
       if(this.amountOfChildrenRegistered <= this.amountOfChildren) {
@@ -108,55 +120,67 @@ export class AddPersonComponent {
         this.hasSentPersonalInformation = true;
         //this.hasOlderChildren = true;
         //this.children.push(this.child);
-        this.addChildForm.reset();
-        if(this.amountOfMovies > 0) {
-          this.hasFavoriteMovies = true;
-          this.amountOfMoviesRegistered = 1;
-          this.hasSentChildInformation = true;
-        }
-        else if(this.amountOfMovies === 0) {
-          //this.children.push(this.child);
-          //this.addChildForm.reset(this.addChildForm.value);
-          this.person.children?.push(this.child);
-          //this.addPersonForm.value.hasOlderChildren = false;
-          this.addChildForm.reset();
-        }
-      }
-      
-      
 
-
-      //Validate if it's the last child
-      else if((this.amountOfChildrenRegistered > this.amountOfChildren)) {
-        //console.log("entre 2");
-        
-        this.hasOlderChildren = false;
-        this.person.children = this.children;
-        //this.children.push(this.child);
         if(this.amountOfMovies > 0) {
           this.hasFavoriteMovies = true;
           this.amountOfMoviesRegistered = 1;
           this.hasSentChildInformation = true;
         } else {
+          this.children.push(this.child);
+        }
+        this.addChildForm.reset();
+        this.addPersonForm.reset();
+      }
+
+
+
+
+      //Validate if it's the last child
+      else {
+        //console.log("entre 2");
+
+        this.hasOlderChildren = false;
+        this.person.children = this.children;
+        this.hasSentChildInformation = true;
+        //this.children.push(this.child);
+        if(this.amountOfMovies > 0) {
+          this.hasFavoriteMovies = true;
+          this.amountOfMoviesRegistered = 1;
+          //this.child = {'name': '', 'lastname': '', 'age': -1, "movies": [], amountOfMovies: 0};
+        } else {
           this.hasSentPersonalInformation = false;
-          //this.people.push(this.person);
+          this.children.push(this.child);
+          this.person.children = this.children;
+          this.people.push(this.person);
           let successMessage = "La persona se ha agregado satisfactoriamente.";
           this.addChildForm.value.hasFavoriteMovies = false;
           this._generalService.setSuccessfulMessage(successMessage);
-          this.hasOlderChildren = false;
-          localStorage.setItem('people',JSON.stringify(this.people));
+          //this.hasOlderChildren = false;
+          //localStorage.setItem('people',JSON.stringify(this.people));
           this.addChildForm.reset();
           this.addPersonForm.reset();
+          this.person = {'name': '', 'lastname' : '', 'children': []};
           this.child = {'name': '', 'lastname': '', 'age': -1, "movies": [], amountOfMovies: 0};
+          this.children = [];
+          //this.people.push(this.person);
           //this.people = [];
         }
-        
-  
+
+
       }
+    } else {
+      if(this.addChildForm.value.age === null || this.addChildForm.value.amountOfMovies === null || this.addChildForm.value.lastname === '' || this.addChildForm.value.name === '') {
+        let errorMessage = "Todos los campos son obligatorios";
+        this._generalService.setErrorMessage(errorMessage);
+      }
+      //console.log(this.addChildForm.invalid);
     }
 
   }
 
+  /**
+   * Method that sends the movie information
+   */
   sendMovieData() {
     if(!this.addMovieForm.invalid) {
       this.movie.name = this.addMovieForm.value.name!;
@@ -164,40 +188,58 @@ export class AddPersonComponent {
       this.movie.year = this.addMovieForm.value.year!;
       this.movie.hasOscar = this.addMovieForm.value.hasOscar!;
       this.movies.push(this.movie);
+      console.log("pelicula:",this.movie);
       this.amountOfMoviesRegistered += 1;
-      //Validate if it's that last movie
+      //Validate if it's the last movie
       if(this.amountOfMoviesRegistered > this.amountOfMovies) {
         this.child.movies = this.movies;
         //Validate if it's the last child
         if(this.amountOfChildrenRegistered > this.amountOfChildren) {
           this.hasSentChildInformation = true;
           this.hasSentPersonalInformation = false;
+          this.hasFavoriteMovies = false;
           let successMessage = "La persona se ha agregado satisfactoriamente.";
           this._generalService.setSuccessfulMessage(successMessage);
           this.addChildForm.value.hasFavoriteMovies = false;
-          localStorage.setItem("people",JSON.stringify(this.people));
-          
+          console.log("el hijo depues de la pelicula:",this.child);
+          this.children.push(this.child);
+          this.person.children = this.children;
+          console.log("persona con pelicula:",this.person);
+          //localStorage.setItem("people",JSON.stringify(this.people));
+          this.people.push(this.person);
+          this.person = {'name': '', 'lastname' : '', 'children': []};
+          this.child = {'name': '', 'lastname': '', 'age': 0, "movies": [], amountOfMovies: 0};
+          this.movie = {'name': '', 'director': '', 'year' : 0, 'hasOscar' : false}
+          this.children = [];
+          this.movies = [];
           this.addMovieForm.reset();
           this.addChildForm.reset();
           this.addPersonForm.reset();
-          
+
         } else {
+          this.children.push(this.child);
           this.hasSentChildInformation = false;
+          this.child = {'name': '', 'lastname': '', 'age': -1, "movies": [], amountOfMovies: 0};
+          this.movie = {'name': '', 'director': '', 'year' : 0, 'hasOscar' : false}
           this.hasOlderChildren = true;
           this.hasFavoriteMovies = false;
           this.addChildForm.value.hasFavoriteMovies = false;
           this.addMovieForm.reset();
           this.addChildForm.reset();
-          
+
 
         }
       } else {
         this.addMovieForm.reset();
+        this.movie = {'name': '', 'director': '', 'year' : 0, 'hasOscar' : false}
       }
     }
     else {
-      console.log("holi");
+      if(this.addMovieForm.value.director === '' || this.addMovieForm.value.name === '' || this.addMovieForm.value.year === null) {
+        let errorMessage = "Todos los campos son obligatorios";
+        this._generalService.setErrorMessage(errorMessage);
+      }
     }
-  } 
+  }
 
 }
